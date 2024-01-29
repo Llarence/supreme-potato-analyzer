81380f6e9a2dd21cd5ad18a2f050547b2f5da21e
Llarence <1010kai.t1010@gmail.com>
2024-01-29 13:31:27 -0800

    rename download path

--- paths.py
+++ paths.py
@@ -5,27 +5,27 @@ path = os.path.dirname(os.path.realpath(__file__))
 key_location = f'{path}/key.txt'
 
 def download_location(year):
-    return f'{path}/data/{year}/blue_cache_data/'
+    return f'{path}/data/{year}/download/'
 def teams_location(year):
-    return f'{path}/data/{year}/blue_cache_data/teams.csv'
+    return f'{path}/data/{year}/download/teams.csv'
 
 def x_offense_location(year):
-    return f'{path}/data/{year}/blue_cache_data/x_offense_location.npy'
+    return f'{path}/data/{year}/download/x_offense_location.npy'
 def x_defense_location(year):
-    return f'{path}/data/{year}/blue_cache_data/x_defense_location.npy'
+    return f'{path}/data/{year}/download/x_defense_location.npy'
 def x_meta_location(year):
-    return f'{path}/data/{year}/blue_cache_data/x_meta_location.npy'
+    return f'{path}/data/{year}/download/x_meta_location.npy'
 def y_location(year):
-    return f'{path}/data/{year}/blue_cache_data/y.npy'
+    return f'{path}/data/{year}/download/y.npy'
 
 def test_x_offense_location(year):
-    return f'{path}/data/{year}/blue_cache_data/test_x_offense_location.npy'
+    return f'{path}/data/{year}/download/test_x_offense_location.npy'
 def test_x_defense_location(year):
-    return f'{path}/data/{year}/blue_cache_data/test_x_defense_location.npy'
+    return f'{path}/data/{year}/download/test_x_defense_location.npy'
 def test_x_meta_location(year):
-    return f'{path}/data/{year}/blue_cache_data/test_x_meta_location.npy'
+    return f'{path}/data/{year}/download/test_x_meta_location.npy'
 def test_y_location(year):
-    return f'{path}/data/{year}/blue_cache_data/test_y.npy'
+    return f'{path}/data/{year}/download/test_y.npy'
 
 def model_location(year):
     return f'{path}/data/{year}/model/'
